7cb2415f39a14793901176b66fb078060b324095
diyahir <diyahirc@gmail.com>
2023-05-24 11:59:20 +0900

    Better naming, interface, grace period

--- src/Guardian.sol
+++ src/Guardian.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/console.sol";
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {IGuardian} from "./IGuardian.sol";
 
 struct LiqChangeNode {
     uint256 nextTimestamp;
@@ -16,24 +17,27 @@ struct TokenRateLimitInfo {
     bool exists;
 }
 
-contract Guardian {
+contract Guardian is IGuardian {
     // Rate limit precision, example 700 = 70% max drawdown per period
     int256 public constant PRECISION = 1000;
 
     // liquidity historacle
-    mapping(address => int256) public tokenLiqHistoracle;
+    mapping(address => int256) public tokenLiquidityHistoracle;
 
     // liquidity in window
-    mapping(address => int256) public tokenLiqWindowAmount;
+    mapping(address => int256) public tokenLiquidityWindowAmount;
 
     // linked list timestamp head
-    mapping(address => uint256) public tokenLiqHead;
+    mapping(address => uint256) public tokenLiquidityHead;
 
     // linked list timestamp tail
-    mapping(address => uint256) public tokenLiqTail;
+    mapping(address => uint256) public tokenLiquidityTail;
 
     // token address -> timestamp -> LiqChangeNode
-    mapping(address => mapping(uint256 => LiqChangeNode)) public tokenLiqChanges;
+    mapping(address => mapping(uint256 => LiqChangeNode)) public tokenLiquidityChanges;
+
+    // List of tokens that are rate limited
+    address[] public tokensGuarded;
 
     // token address -> token struct
     mapping(address => TokenRateLimitInfo) public tokensRateLimitInfo;
@@ -46,21 +50,24 @@ contract Guardian {
 
     bool public isRateLimited;
 
+    uint256 public rateLimitCooldownPeriod;
+
+    uint256 public lastRateLimitTimestamp;
+
+    uint256 public gracePeriod = 3 hours;
+
+    // Guarded contracts
     mapping(address => bool) public isGuarded;
+    // List of guarded contracts
+    address[] public guardedContracts;
 
-    modifier onlyGuarded() {
-        require(_isGuardedContract(msg.sender), "Only guarded contracts");
-        _;
-    }
-
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin");
-        _;
-    }
-
-    constructor(address _admin, address _guardedContracts) {
+    // gracePeriod refers to the time after a rate limit breech and then overriden where withdrawals are still allowed
+    // for example a false positive rate limit breech, then it is overrident, so withdrawals are still allowed for a period of time
+    // before the rate limit is enforced again, it should be set to be at least your largest withdrawalPeriod length
+    constructor(address _admin, uint256 _rateLimitCooldownPeriod, uint256 _gracePeriod) {
         admin = _admin;
-        isGuarded[_guardedContracts] = true;
+        rateLimitCooldownPeriod = _rateLimitCooldownPeriod;
+        gracePeriod = _gracePeriod;
     }
 
     function registerToken(
@@ -75,14 +82,7 @@ contract Guardian {
         token.withdrawalRateLimitPerPeriod = _withdrawalRateLimitPerPeriod;
         token.withdrawalPeriod = _withdrawalPeriod;
         token.bootstrapAmount = _bootstrapAmount;
-    }
-
-    function overrideLimit() external onlyAdmin {
-        isRateLimited = false;
-    }
-
-    function transferAdmin(address _admin) external onlyAdmin {
-        admin = _admin;
+        tokensGuarded.push(_tokenAddress);
     }
 
     // give guarded contracts one function to call for convenience
@@ -90,20 +90,15 @@ contract Guardian {
         _recordTokenChange(_tokenAddress, _amount, true);
     }
 
-    // Allow users to claim locked funds when rate limit is resolved
-    function claimLockedFunds(address _tokenAddress) external {
-        require(lockedFunds[msg.sender][_tokenAddress] > 0, "No locked funds");
-        require(!isRateLimited, "Rate limited");
-        IERC20 erc20Token = IERC20(_tokenAddress);
-        erc20Token.transfer(msg.sender, lockedFunds[msg.sender][_tokenAddress]);
-        lockedFunds[msg.sender][_tokenAddress] = 0;
-    }
-
-    function withdraw(address _tokenAddress, uint256 _amount, address _recipient) external {
-        TokenRateLimitInfo storage token = tokensRateLimitInfo[_tokenAddress];
+    function withdraw(
+        address _tokenAddress,
+        uint256 _amount,
+        address _recipient
+    ) external onlyGuarded {
+        TokenRateLimitInfo storage tokenRlInfoInfo = tokensRateLimitInfo[_tokenAddress];
 
         // Check if the token has enforced rate limited
-        if (!token.exists) {
+        if (!tokenRlInfoInfo.exists) {
             // if it is not rate limited, just transfer the tokens
             IERC20 erc20TokenNoLimit = IERC20(_tokenAddress);
             erc20TokenNoLimit.transfer(_recipient, _amount);
@@ -117,10 +112,12 @@ contract Guardian {
             return;
         }
 
-        // Check if rate limit is breeched after withdrawal
-        if (checkIfRateLimitBreeched(_tokenAddress)) {
+        // Check if rate limit is breeched after withdrawal and not in grace period
+        // (grace period allows for withdrawals to be made if rate limit is breeched but overriden)
+        if (checkIfRateLimitBreeched(_tokenAddress) && !isInGracePeriod()) {
             // if it is, set rate limited to true
             isRateLimited = true;
+            lastRateLimitTimestamp = block.timestamp;
             // add to locked funds claimable when resolved
             lockedFunds[_recipient][_tokenAddress] += _amount;
             return;
@@ -131,17 +128,47 @@ contract Guardian {
         erc20Token.transfer(_recipient, _amount);
     }
 
-    function addGuardedContracts(address[] calldata _guardedContracts) public {
-        require(msg.sender == admin, "Only admin");
+    function overrideLimit() external onlyAdmin {
+        isRateLimited = false;
+    }
+
+    function transferAdmin(address _admin) external onlyAdmin {
+        admin = _admin;
+    }
+
+    // Allow users to claim locked funds when rate limit is resolved
+    function claimLockedFunds(address _tokenAddress) external {
+        require(lockedFunds[msg.sender][_tokenAddress] > 0, "No locked funds");
+        require(!isRateLimited, "Rate limited");
+        IERC20 erc20Token = IERC20(_tokenAddress);
+        erc20Token.transfer(msg.sender, lockedFunds[msg.sender][_tokenAddress]);
+        lockedFunds[msg.sender][_tokenAddress] = 0;
+    }
+
+    function overrideExpiredRateLimit() public {
+        require(isRateLimited, "Not rate limited");
+        require(
+            block.timestamp - lastRateLimitTimestamp >= rateLimitCooldownPeriod,
+            "Cooldown period not reached"
+        );
+        isRateLimited = false;
+    }
+
+    function addGuardedContracts(address[] calldata _guardedContracts) public onlyAdmin {
         for (uint256 i = 0; i < _guardedContracts.length; i++) {
             isGuarded[_guardedContracts[i]] = true;
+            guardedContracts.push(_guardedContracts[i]);
         }
     }
 
-    function removeGuardedContracts(address[] calldata _guardedContracts) public {
-        require(msg.sender == admin, "Only admin");
+    function removeGuardedContracts(address[] calldata _guardedContracts) public onlyAdmin {
         for (uint256 i = 0; i < _guardedContracts.length; i++) {
             isGuarded[_guardedContracts[i]] = false;
+            for (uint256 j = 0; j < guardedContracts.length; j++) {
+                if (guardedContracts[j] == _guardedContracts[i]) {
+                    delete guardedContracts[j];
+                }
+            }
         }
     }
 
@@ -150,16 +177,14 @@ contract Guardian {
         if (!token.exists) {
             return false;
         }
-        int256 currentLiq = tokenLiqHistoracle[_tokenAddress];
+        int256 currentLiq = tokenLiquidityHistoracle[_tokenAddress];
 
         // Only enforce rate limit if there is significant liquidity
         if (token.bootstrapAmount > uint(currentLiq)) {
             return false;
         }
 
-        int256 currentWindow = tokenLiqWindowAmount[_tokenAddress];
-
-        int256 futureLiq = currentLiq + currentWindow;
+        int256 futureLiq = currentLiq + tokenLiquidityWindowAmount[_tokenAddress];
         int256 minLiq = (currentLiq * token.withdrawalRateLimitPerPeriod) / PRECISION;
 
         return futureLiq < minLiq;
@@ -170,10 +195,10 @@ contract Guardian {
         uint256 _amount,
         bool _isPositive
     ) internal onlyGuarded {
-        TokenRateLimitInfo storage tokenRL = tokensRateLimitInfo[_tokenAddress];
+        TokenRateLimitInfo storage tokenRlInfo = tokensRateLimitInfo[_tokenAddress];
 
         // If token does not have a rate limit, do nothing
-        if (!tokenRL.exists) {
+        if (!tokenRlInfo.exists) {
             return;
         }
 
@@ -186,33 +211,35 @@ contract Guardian {
         if (!_isPositive) {
             newLiqChange.amount = -int(_amount);
         }
-        tokenLiqWindowAmount[_tokenAddress] += newLiqChange.amount;
+        tokenLiquidityWindowAmount[_tokenAddress] += newLiqChange.amount;
 
         // if there is no head, set the head to the new inflow
-        if (tokenLiqHead[_tokenAddress] == 0) {
-            tokenLiqHead[_tokenAddress] = block.timestamp;
-            tokenLiqTail[_tokenAddress] = block.timestamp;
-            tokenLiqChanges[_tokenAddress][block.timestamp] = newLiqChange;
+        if (tokenLiquidityHead[_tokenAddress] == 0) {
+            tokenLiquidityHead[_tokenAddress] = block.timestamp;
+            tokenLiquidityTail[_tokenAddress] = block.timestamp;
+            tokenLiquidityChanges[_tokenAddress][block.timestamp] = newLiqChange;
         } else {
             // if there is a head, check if the new inflow is within the period
             // if it is, add it to the head
             // if it is not, add it to the tail
-            if (block.timestamp - tokenLiqHead[_tokenAddress] >= tokenRL.withdrawalPeriod) {
+            if (
+                block.timestamp - tokenLiquidityHead[_tokenAddress] >= tokenRlInfo.withdrawalPeriod
+            ) {
                 _traverseLinkedListUntilInPeriod(_tokenAddress, block.timestamp);
             }
 
             // check if tail is the same as block.timestamp (multiple txs in same block)
-            if (tokenLiqTail[_tokenAddress] == block.timestamp) {
+            if (tokenLiquidityTail[_tokenAddress] == block.timestamp) {
                 // add amount
-                tokenLiqChanges[_tokenAddress][block.timestamp].amount += newLiqChange.amount;
+                tokenLiquidityChanges[_tokenAddress][block.timestamp].amount += newLiqChange.amount;
             } else {
                 // add to tail
-                LiqChangeNode storage tail = tokenLiqChanges[_tokenAddress][
-                    tokenLiqTail[_tokenAddress]
+                LiqChangeNode storage tail = tokenLiquidityChanges[_tokenAddress][
+                    tokenLiquidityTail[_tokenAddress]
                 ];
                 tail.nextTimestamp = block.timestamp;
-                tokenLiqChanges[_tokenAddress][block.timestamp] = newLiqChange;
-                tokenLiqTail[_tokenAddress] = block.timestamp;
+                tokenLiquidityChanges[_tokenAddress][block.timestamp] = newLiqChange;
+                tokenLiquidityTail[_tokenAddress] = block.timestamp;
             }
         }
     }
@@ -220,32 +247,46 @@ contract Guardian {
     // Traverse the linked list from the head until the timestamp is within the period
     function _traverseLinkedListUntilInPeriod(address _tokenAddress, uint256 _timestamp) internal {
         int256 totalChange = 0;
-        uint256 currentHeadTimestamp = tokenLiqHead[_tokenAddress];
+        uint256 currentHeadTimestamp = tokenLiquidityHead[_tokenAddress];
         while (
             currentHeadTimestamp != 0 &&
             _timestamp - currentHeadTimestamp >= tokensRateLimitInfo[_tokenAddress].withdrawalPeriod
         ) {
-            LiqChangeNode memory node = tokenLiqChanges[_tokenAddress][currentHeadTimestamp];
+            LiqChangeNode memory node = tokenLiquidityChanges[_tokenAddress][currentHeadTimestamp];
             totalChange += node.amount;
             // Clear data
-            delete tokenLiqChanges[_tokenAddress][currentHeadTimestamp];
+            delete tokenLiquidityChanges[_tokenAddress][currentHeadTimestamp];
 
-            currentHeadTimestamp = tokenLiqChanges[_tokenAddress][currentHeadTimestamp]
+            currentHeadTimestamp = tokenLiquidityChanges[_tokenAddress][currentHeadTimestamp]
                 .nextTimestamp;
         }
         // Set new head, if there is no head, set it to the current timestamp
         if (currentHeadTimestamp == 0) {
-            tokenLiqHead[_tokenAddress] = _timestamp;
+            tokenLiquidityHead[_tokenAddress] = _timestamp;
         } else {
-            tokenLiqHead[_tokenAddress] = currentHeadTimestamp;
+            tokenLiquidityHead[_tokenAddress] = currentHeadTimestamp;
         }
         // update historacle
-        tokenLiqHistoracle[_tokenAddress] += totalChange;
+        tokenLiquidityHistoracle[_tokenAddress] += totalChange;
         // update window
-        tokenLiqWindowAmount[_tokenAddress] -= totalChange;
+        tokenLiquidityWindowAmount[_tokenAddress] -= totalChange;
+    }
+
+    function isInGracePeriod() public view returns (bool) {
+        return block.timestamp - lastRateLimitTimestamp <= gracePeriod && !isRateLimited;
     }
 
     function _isGuardedContract(address _input) internal view returns (bool) {
         return isGuarded[_input];
+    }
+
+    modifier onlyGuarded() {
+        require(_isGuardedContract(msg.sender), "Only guarded contracts");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin");
+        _;
     }
 }
--- src/MockDeFiProtocol.sol
+++ src/MockDeFiProtocol.sol
@@ -2,23 +2,26 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/console.sol";
-import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
-import {Guardian} from "./Guardian.sol";
+import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+import {IGuardian} from "./IGuardian.sol";
 
 contract MockDeFi {
-    Guardian guardian;
+    using SafeERC20 for IERC20;
+    IGuardian guardian;
 
     function setGuardian(address _guardian) external {
-        guardian = Guardian(_guardian);
+        guardian = IGuardian(_guardian);
     }
 
     function deposit(address token, uint256 amount) external {
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
         guardian.recordInflow(token, amount);
     }
 
     function withdraw(address token, uint256 amount) external {
-        IERC20(token).transfer(address(guardian), amount);
+        IERC20(token).safeTransfer(address(guardian), amount);
         guardian.withdraw(token, amount, msg.sender);
     }
 
